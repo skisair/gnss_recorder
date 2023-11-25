224c5ec9aa0c89a054d326e395e01e557c174f65
Shohei Watanabe <watanabe.shohei@brains-tech.co.jp>
2023-11-26 08:02:35 +0900

    #2 SQLITE テーブル作成（途中）

--- sqlite_recorder.py
+++ sqlite_recorder.py
@@ -13,6 +13,51 @@ connection = sqlite3.connect(db_name)
 cursor = connection.cursor()
 
 # GPRMC,GPGGA,GPVTG,GPGSA,GPGSV,GPGLL,GPTXT
-cursor.execute("CREATE TABLE IF NOT EXISTS GPRMC(id integer,name text)")
+# 共通：　device_id, local_time, data_id,
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS GPRMC(
+        device_id TEXT, 
+        local_time TEXT, 
+        data_id TEXT, 
+        gps_date_time TEXT, 
+        lat REAL, 
+        lon REAL, 
+        speed REAL, 
+        mode TEXT
+    )
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS GPGGA(
+        device_id TEXT, 
+        local_time TEXT, 
+        data_id TEXT, 
+        gps_date_time TEXT, 
+        lat REAL, 
+        lon REAL, 
+        fix_quality　INTEGER,
+        num_satellites INTEGER,
+        hdop REAL, 
+        altitude REAL,
+        geoid_height REAL
+    )
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS GPVTG(
+        device_id TEXT, 
+        local_time TEXT, 
+        data_id TEXT, 
+        m_course TEXT,
+        speed REAL,
+        mode TEXT
+    )
+    """
+)
 
 connection.close()
